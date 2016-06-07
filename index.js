6762dca61ab0e06fd1b7b7b0bbc1fbb60c46ac44
megastef <stefan.thies@sematext.com>
2016-06-07 09:52:51 +0200

    move to new slack-client, and logsene-js for direct indexing without logagent

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 #!/usr/bin/env node
 // API Token from https://api.slack.com/web
-function slackClient (apiToken) {
+function slackClient (apiToken, logseneToken) {
   var Slack, autoMark, autoReconnect, slack, slackToken
 
-  Slack = require('slack-client')
+  Slack = require('@slack/client').RtmClient
   slackToken = apiToken; autoReconnect = true
   autoMark = true
+  slack = new Slack(slackToken, {logLevel: 'error'})
+  var Logsene = require('logsene-js') 
+  var logger =  new Logsene (logseneToken)
+  //new Slack(slackToken, autoReconnect, autoMark)
 
-  slack = new Slack(slackToken, autoReconnect, autoMark)
-
-  slack.on('open', function () {
+  slack.on('authenticated', function (slack) {
     return console.log('Connected to ' + slack.team.name + ' as @' + slack.self.name)
   })
 
-  slack.on('message', function (message) {
-    var channel = slack.getChannelGroupOrDMByID(message.channel)
-    var u = slack.getUserByID(message.user)
+  slack.on('message' , function (message) {
+    var channel = slack.dataStore.getChannelGroupOrDMById(message.channel);
+
+    //var channel = slack.getChannelGroupOrDMByID(message.channel)
+    var u = slack.dataStore.getUserById(message.user)
     if (!u) {
       u = {}
     }
     if (!u.profile) {
       u.profile = {}
     }
-    return console.log(
-      JSON.stringify({
-        userName: u.name,
-        userImage: u.profile.image_48,
-        userFirstName: u.profile.first_name,
-        userLastName: u.profile.last_name,
-        channel: channel.name,
-        message: message.text,
-        '@timestamp': new Date()}))
+    var msg = {
+      userName: u.name,
+      userImage: u.profile.image_48,
+      userFirstName: u.profile.first_name,
+      userLastName: u.profile.last_name,
+      channel: channel.name,
+      message: message.text,
+      '@timestamp': new Date()
+    }
+    logger.log ('info', msg.message, msg)
+    console.log(msg)
   })
 
   slack.on('error', function (err) {
@@ -45,4 +51,9 @@ if(!process.argv[2]) {
   process.exit(1)
 }
 
-slackClient(process.argv[2])
+if(!process.argv[3]) {
+  console.error('Missing Logsene APP token')
+  process.exit(1)
+}
+
+slackClient(process.argv[2], process.argv[3])
